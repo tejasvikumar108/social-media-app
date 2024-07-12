4f359415e12dddb9a51074caa52236359f269c43
Tejasvi Kumar Thakur <tejasvikumar108@gmail.com>
2024-07-12 09:14:42 +0530

    first

---
+++ SOCIALMEDIAAPP/BED/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/validationRoutes");
+const postRoutes = require("./routes/postRoutes");
+
+const PORT = 5000;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb+srv://tejasvikumar108:tejasvi108@cluster0.p5saiiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+app.use('/socialMedia/user',userRoutes);
+app.use('/socialMedia/authToken', authRoutes);
+app.use('/socialMedia/posts', postRoutes);
+
+app.listen(PORT,()=>console.log(`Server is running at port: ${PORT}`))
